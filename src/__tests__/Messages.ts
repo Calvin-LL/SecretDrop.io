66a676fc2fea083dc21283de447ee00cd950a22e
Calvin Liang <me@calvin.sh>
2020-01-31 00:10:43 -0800

    Add test for different hash types

--- src/__tests__/Messages.ts
+++ src/__tests__/Messages.ts
@@ -1,28 +1,66 @@
 import EncryptedMessage from "../common/typescript/EncryptedMessage";
 import KeyPair from "../common/typescript/KeyPair";
+import { KeyPairConfig } from "../common/typescript/KeyPair";
 import PlainMessage from "../common/typescript/PlainMessage";
 import WebCrypto from "node-webcrypto-ossl";
+// @ts-ignore
 import blns from "./blns.json";
 
-describe("PlainMessage to and from EncryptedMessage", () => {
-  // @ts-ignore
-  window.crypto = new WebCrypto();
+// @ts-ignore
+window.crypto = new WebCrypto();
 
-  test("valid strings from big-list-of-naughty-strings", async () => {
+describe("PlainMessage to and from EncryptedMessage", () => {
+  test("valid strings from big-list-of-naughty-strings with default hash", async () => {
+    await encryptThenDecryptWithKey();
+  }, 120000);
+
+  test("valid strings from big-list-of-naughty-strings with SHA-256", async () => {
+    await encryptThenDecryptWithKey({ hash: "SHA-256" });
+  }, 120000);
+
+  test("valid strings from big-list-of-naughty-strings with SHA-384", async () => {
+    await encryptThenDecryptWithKey({ hash: "SHA-384" });
+  }, 120000);
+
+  test("valid strings from big-list-of-naughty-strings with SHA-512", async () => {
+    await encryptThenDecryptWithKey({ hash: "SHA-512" });
+  }, 120000);
+
+  test("invalid strings from big-list-of-naughty-strings", async () => {
     for (const message of blns as string[]) {
       const keyPair = new KeyPair();
       await keyPair.init();
 
-      const privateKey = await keyPair.getPrivateKey();
       const publicKey = await keyPair.getPublicKey();
 
-      const plainMessage = new PlainMessage(message, publicKey);
-      const encryptedPlainMessage = await plainMessage.encrypt();
+      if (publicKey.isStringEncryptable(message)) continue;
 
-      const encryptedMessage = new EncryptedMessage(encryptedPlainMessage, privateKey);
-      const decryptedEncryptedPlainMessage = await encryptedMessage.decrypt();
+      const encryptFunction = async () => {
+        const plainMessage = new PlainMessage(message, publicKey);
+        await plainMessage.encrypt(); // should throw
+      };
 
-      expect(decryptedEncryptedPlainMessage).toBe(message);
+      await expect(encryptFunction()).rejects.toThrow();
     }
   }, 120000);
 });
+
+async function encryptThenDecryptWithKey(config?: KeyPairConfig) {
+  for (const message of blns as string[]) {
+    const keyPair = new KeyPair(config);
+    await keyPair.init();
+
+    const privateKey = await keyPair.getPrivateKey();
+    const publicKey = await keyPair.getPublicKey();
+
+    if (!publicKey.isStringEncryptable(message)) continue;
+
+    const plainMessage = new PlainMessage(message, publicKey);
+    const encryptedPlainMessage = await plainMessage.encrypt();
+
+    const encryptedMessage = new EncryptedMessage(encryptedPlainMessage, privateKey);
+    const decryptedEncryptedPlainMessage = await encryptedMessage.decrypt();
+
+    expect(decryptedEncryptedPlainMessage).toBe(message);
+  }
+}
