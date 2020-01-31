5d405f5cf41f32af7264e53ac890dcd90bab368b
Calvin Liang <me@calvin.sh>
2020-01-31 15:55:27 -0800

    Add tests for Keys

--- src/__tests__/Keys.test.ts
+++ src/__tests__/Keys.test.ts
@@ -42,7 +42,7 @@ describe("Export and import public key", () => {
   }
 });
 
-describe("Export private key", () => {
+describe("Export and import private key", () => {
   test("with default hash", async () => {
     await testPrivateKeyWithHash();
   }, 120000);
@@ -75,4 +75,76 @@ describe("Export private key", () => {
 
     expect(privateKeyString).toBe(privateKey2String);
   }
+});
+
+describe("KeyPair without init", () => {
+  test("getPrivateKey", async () => {
+    const keyPair = new KeyPair();
+
+    await expect(keyPair.getPrivateKey()).rejects.toBeTruthy();
+  }, 120000);
+
+  test("getPublicKey", async () => {
+    const keyPair = new KeyPair();
+
+    await expect(keyPair.getPublicKey()).rejects.toBeTruthy();
+  }, 120000);
+});
+
+describe("PrivateKey", () => {
+  test("improper key string", async () => {
+    const privateKey = new PrivateKey("invalid string");
+
+    await expect(privateKey.init()).rejects.toBeTruthy();
+  }, 120000);
+
+  test("init without key string", async () => {
+    const privateKey = new PrivateKey();
+
+    await expect(privateKey.init()).rejects.toBeTruthy();
+  }, 120000);
+
+  test("getKeyString without init", async () => {
+    const privateKey = new PrivateKey();
+
+    await expect(privateKey.getKeyString()).rejects.toBeTruthy();
+  }, 120000);
+
+  test("decryptString without init", async () => {
+    const privateKey = new PrivateKey();
+
+    await expect(privateKey.decryptString("string to encrypt")).rejects.toBeTruthy();
+  }, 120000);
+});
+
+describe("PublicKey", () => {
+  test("improper key string", async () => {
+    const publicKey = new PublicKey("invalid string");
+
+    await expect(publicKey.init()).rejects.toBeTruthy();
+  }, 120000);
+
+  test("init without key string", async () => {
+    const publicKey = new PublicKey();
+
+    await expect(publicKey.init()).rejects.toBeTruthy();
+  }, 120000);
+
+  test("getKeyString without init", async () => {
+    const publicKey = new PublicKey();
+
+    await expect(publicKey.getKeyString()).rejects.toBeTruthy();
+  }, 120000);
+
+  test("decryptString without init", async () => {
+    const publicKey = new PublicKey();
+
+    await expect(publicKey.encryptString("string to encrypt")).rejects.toBeTruthy();
+  }, 120000);
+
+  test("getMaxStringLength without init", async () => {
+    const publicKey = new PublicKey();
+
+    expect(() => publicKey.getMaxStringLength()).toThrow();
+  });
 });
