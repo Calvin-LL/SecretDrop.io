31624453fe240c3b609dfdc8270dc124b30bae16
Calvin Liang <me@calvin.sh>
2020-02-17 18:09:43 -0800

    Add blank help and decrypt page

---
+++ src/help/webpack.prod.config.js
@@ -0,0 +1,11 @@
+const common = require("../../webpack.prod.common.config");
+const path = require("path");
+
+module.exports = {
+  ...common,
+  entry: path.join(__dirname, "index.ts"),
+  output: {
+    ...common.output,
+    path: path.join(__dirname, "..", "..", "dist", "help"),
+  },
+};
