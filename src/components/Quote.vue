250876fd2a0ab663f41039a917153a75aa787fa8
Calvin Liang <me@calvin.sh>
2020-04-29 17:57:49 -0700

    Add faq

---
+++ src/components/Quote.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="quote">
+    <slot></slot>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+
+export default Vue.extend({
+  name: "Quote",
+});
+</script>
+
+<style scoped lang="scss">
+.quote {
+  border-radius: 4px;
+
+  padding: 8px;
+
+  background-color: rgba($color: #000, $alpha: 0.1);
+
+  @media (prefers-color-scheme: dark) {
+    background-color: rgba($color: #fff, $alpha: 0.1);
+  }
+}
+</style>
