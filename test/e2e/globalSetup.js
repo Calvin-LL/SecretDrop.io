260f645e538cc295bedb2444ed20666c76d28518
Calvin Liang <me@calvin.sh>
2020-05-31 14:37:39 -0700

    Use dist for CI tests

--- test/e2e/globalSetup.js
+++ test/e2e/globalSetup.js
@@ -3,15 +3,28 @@ const { PORT } = require("./constants");
 const { setup } = require("jest-dev-server");
 
 module.exports = async function () {
-  await setup([
-    {
-      command: `vue-cli-service serve --port ${PORT} --mode production`,
-      launchTimeout: 60000,
-      port: PORT,
-      options: {
-        cwd: path.join(__dirname, "..", ".."),
-        env: { ...process.env, TEST: true, NODE_ENV: "production" },
+  if (process.env.TEST_WITH_DIST) {
+    await setup([
+      {
+        command: `serve ./dist -l ${PORT}`,
+        launchTimeout: 60000,
+        port: PORT,
+        options: {
+          cwd: path.join(__dirname, "..", ".."),
+        },
       },
-    },
-  ]);
+    ]);
+  } else {
+    await setup([
+      {
+        command: `vue-cli-service serve --port ${PORT} --mode production`,
+        launchTimeout: 60000,
+        port: PORT,
+        options: {
+          cwd: path.join(__dirname, "..", ".."),
+          env: { ...process.env, TEST: true, NODE_ENV: "production" },
+        },
+      },
+    ]);
+  }
 };
