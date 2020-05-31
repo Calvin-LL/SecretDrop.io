c7252eac3c0bbfa4c0a32b778017d879ceb0e37f
Calvin Liang <me@calvin.sh>
2020-05-31 15:53:54 -0700

    Remove travis

--- test/e2e/main.test.ts
+++ test/e2e/main.test.ts
@@ -618,8 +618,8 @@ function testSnapshot(
               customSnapshotsDir: path.join(
                 __dirname,
                 "__image_snapshots__",
-                process.env.TRAVIS && process.platform === "darwin"
-                  ? `travis_${process.platform}`
+                process.env.CI && process.platform === "darwin"
+                  ? `ci_${process.platform}`
                   : process.platform,
                 browserType,
                 `${viewport.width}×${viewport.height}`,
@@ -628,8 +628,8 @@ function testSnapshot(
               customDiffDir: path.join(
                 __dirname,
                 "__image_snapshots__",
-                process.env.TRAVIS && process.platform === "darwin"
-                  ? `travis_${process.platform}`
+                process.env.CI && process.platform === "darwin"
+                  ? `ci_${process.platform}`
                   : process.platform,
                 "__diff_output__",
                 browserType,
