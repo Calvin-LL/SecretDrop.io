bf0d3d6a7d5007fa5d97ef3534ae71db1b4f8a6d
Calvin Liang <me@calvin.sh>
2021-03-10 15:24:23 -0800

    chore: update packages and remove resize-observer-browser
    Remove resize-observer-browser because typescript has it now

--- src/core/CryptoCheck.ts
+++ src/core/CryptoCheck.ts
@@ -8,7 +8,9 @@ export default function isCryptoUseable() {
   return (
     window.crypto &&
     window.crypto.subtle &&
+    // @ts-expect-error typescript thinks this is always true
     window.crypto.subtle.importKey &&
+    // @ts-expect-error typescript thinks this is always true
     window.crypto.subtle.deriveKey &&
     !(browser?.name === "edge") &&
     !(browser?.name === "ie")
